a66f1394ad71c8cb754407141030160560768ca8
Minsu Kang <bluehawhy@gmail.com>
2022-08-25 13:18:14 +0900

    코드 약간 정리 및 개선 사항 정리

--- python/12865.py
+++ python/12865.py
@@ -1,24 +1,15 @@
+import random
+import sys
 from itertools import permutations
 
-def make_dict_by_max_value(weight_list,value_list):
-    dict_max = {}
-    for i in range(len(weight_list)):
-        weight = weight_list[i]
-        value = value_list[i]
-        if weight in dict_max:
-            value_in_dict = dict_max[weight]
-            if value_in_dict < value:
-                dict_max[weight] = value
-        else:
-            dict_max[weight] = value
-    return dict_max
-
-def find_reasonable_value(weights,weight_list,dict_max):
-    acceptable_list = []
-    weight_list.sort()
+def find_reasonable_value(weights,dict_max):
+    #acceptable_list = []
     value = 0
-    for i in range(len(weight_list)):
-        combi_weights = list(permutations(weight_list, i))
+    #여기서 DP를 사용해야 함.
+    #모든 조합은 메모리나 시간초과에 많은 비용이 듬
+    for i in range(len(dict_max.keys())):
+        combi_weights = list(permutations(dict_max.keys(), i+1))
+        print(combi_weights)
         for combi_weight in combi_weights:
             if sum(combi_weight) > weights:
                 pass
@@ -31,25 +22,46 @@ def find_reasonable_value(weights,weight_list,dict_max):
                     temp_value = temp_value + dict_max[i]
                     if temp_value > value:
                         value = temp_value
-                        acceptable_list = list(combi_weight)
+                        #acceptable_list = list(combi_weight)
     #print(acceptable_list)
     #print(value)
     return value
 
 
-def make_list_wight_values():
-    input_count = input()
-    items,weights = map(int, input_count.split())
-    weight_list = [] #무게들을 조합
-    value_list = []
+def get_list_wight_values():
+    items,weights = map(int, sys.stdin.readline().split())
+    dict_max = {}    
     for i in range(items):
-        weight, value = map(int, input().split())
-        weight_list.append(weight)
-        value_list.append(value)
-    #print(weight_list,value_list)
-    total_val = find_reasonable_value(weights,weight_list,make_dict_by_max_value(weight_list,value_list))
+        weight, value = map(int, sys.stdin.readline().split())
+        #print(weight, value)
+        #print(dict_max)
+        if weight not in dict_max.keys():
+            #print('weight not in dict')
+            dict_max[weight]= value            
+        else:
+            #print('weight in dict')
+            value_in_dict = dict_max[weight]
+            if value >= value_in_dict:
+                dict_max[weight] = value
+    #print(dict_max)
+    return weights, dict_max
+
+def make_list_wight_values():
+    weight_dicmax = get_list_wight_values()
+    total_val = find_reasonable_value(weight_dicmax[0],weight_dicmax[1])
     print(total_val)
     return 0
 
 
-make_list_wight_values()
+#make_list_wight_values()
+
+items = 3
+weights = 2
+dict_max = {}
+for i in range(items):
+    dict_max[i+1] = random.randint(0,100)
+print(dict_max)
+value = find_reasonable_value(weights, dict_max)
+print(value)
+#combi_weights = list(permutations(dict_max.keys(), 50))
+#print(sys.getsizeof(combi_weights))
